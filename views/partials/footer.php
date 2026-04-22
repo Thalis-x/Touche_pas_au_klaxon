f1e3f12400c523b0b581f528135f377edf479ce0
thalis <sarra_borea@outlook.com>
2026-04-22 13:08:48 +0200

    feat:Ajout de la vue footer #5

---
+++ views/partials/footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Footer de l'application.
+ * Variables disponibles : $copyright
+ */
+
+use App\Core\View;
+?>
+<footer class="text-center py-4 mt-auto border-top">
+    <p class="mb-0"><?= View::e($copyright) ?></p>
+</footer>
